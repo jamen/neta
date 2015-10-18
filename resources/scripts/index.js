6c63cabb792b1f3ff0b6f4d25024b17901f53124
Jamen Marz <jamen.marz@gmail.com>
2015-10-18 02:59:09 -0600

    Change back to original: Animations should depend on the DE.

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -6,18 +6,7 @@ const $ = require('jquery'),
 let main = remote.getCurrentWindow();
 
 $(function(){
-  $('.tools .close').on('click', function(){
-    $('.app').addClass('closing');
-    setTimeout(function(){
-      main.close();
-    }, 300);
-  });
-
-  $('.tools .maximize').on('click', function(){
-    main.maximize();
-  });
-
-  $('.tools .minimize').on('click', function(){
-    main.minimize();
+  $('.tools > div').on('click', function(e){
+    main[e.target.className]();
   });
 });
