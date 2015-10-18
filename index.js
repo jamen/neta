9f0e40a6a70eed80f17f9b6b20e400dfab2ace8c
Jamen Marz <jamen.marz@gmail.com>
2015-10-18 02:03:11 -0600

    Update for new application

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-<!doctype html>
-<html>
-</html>
+'use strict';
+
+const app = require('app'),
+      BW = require('browser-window'),
+      path = require('path');
+
+// Various tasks to do before the app is ready.
+require('crash-reporter').start();
+app.commandLine.appendSwitch('enable-transparent-visuals');
+let main = null;
+
+
+app.on('ready', function(){
+  const screen = require('screen');
+
+  let size = screen.getPrimaryDisplay().workAreaSize;
+
+  main = new BW({
+    'width': size.width - 200,
+    'height': size.height - 200,
+    'frame': false,
+    'transparent': true,
+    'center': true
+  });
+
+  main.loadUrl('file://' + path.join(__dirname, 'views', 'index.html'));
+});
