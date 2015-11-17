d094fc661d26f691a323657f0b71b2f38ac15436
Jamen Marz <jamenmarz@gmail.com>
2015-11-17 00:44:12 -0700

    Move style import to app-window

--- index.js
+++ index.js
@@ -2,17 +2,14 @@
 
 const app = require('app'),
       path = require('path'),
-      window = require('./scripts/app-window'),
-      init = require('./scripts/init');
+      window = require('./scripts/app-window');
 
 // Various tasks to do before the app is ready.
 require('crash-reporter').start();
 app.commandLine.appendSwitch('enable-transparent-visuals');
-const socii = init();
 
 // App
 app.on('ready', function(){
   const main = window(path.join(__dirname, 'index.html')), wc = main.webContents;
-  wc.insertCSS(socii.style);
   main.openDevTools({detach: true});
 });
--- scripts/app-window.js
+++ scripts/app-window.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const BrowserWindow = require('browser-window'),
+      init = require('./init'),
       arg = item => process.argv.indexOf(item) !== -1;
 
 module.exports = function(file){
   const screen = require('screen'),
         size = screen.getPrimaryDisplay().workAreaSize,
+        socii = init(),
 
   is = {
     'simple': arg('--simple'),
     'dev': arg('--dev'),
-    'menu': arg('--menu')
+    'menu': arg('--menu'),
+    'disableStyle': arg('--disable-style')
   },
 
   app = new BrowserWindow({
@@ -26,6 +29,7 @@ module.exports = function(file){
   }), wc = app.webContents;
 
   if (!is.menu) app.setMenu(null);
+  if (!is.disableStyle) wc.insertCSS(socii.style);
   if (is.dev) app.openDevTools();
   if (is.simple) wc.on('dom-ready', () => wc.send('simple'));
 
