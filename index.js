f52e1bd836f89abc7d21b12e56f9758e44872e15
Jamen Marz <jamenmarz@gmail.com>
2015-12-13 18:35:45 -0700

    Fresh start

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-const app = require('app'),
-      path = require('path'),
-      window = require('./scripts/app-window');
-
-// Various tasks to do before the app is ready.
-require('crash-reporter').start();
-app.commandLine.appendSwitch('enable-transparent-visuals');
-
-// App
-app.on('ready', function(){
-  const main = window(path.join(__dirname, 'index.html')), wc = main.webContents;
-});
+/* index.js
+ * Main initiation file of Socii.
+ * * */
