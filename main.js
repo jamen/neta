4eb581479e1cab5bbc48a1ef80a4de42901767de
Jamen Marz <jamenmarz@gmail.com>
2015-12-14 05:35:49 -0700

    Add some other basics

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import campfire from './lib';
+import app from 'app';
+import BrowserWindow from 'browser-window';
 
 /* main.js
  * Main initiation file of campfire.
  * * */
+
+app.on('ready', () => {
+
+});
