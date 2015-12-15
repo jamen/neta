af5b8bd9ef125893c61bafa6cc1601a61bd89da8
Jamen Marz <jamenmarz@gmail.com>
2015-12-15 12:04:15 -0700

    Hide errors for now

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import campfire from './lib';
 import app from 'app';
-import BrowserWindow from 'browser-window';
+// import campfire from './lib';
+// import BrowserWindow from 'browser-window';
 
 /* main.js
  * Main initiation file of campfire.
