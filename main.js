56a594a2ed233791f5d3a20dce069319aa0ef2e6
Jamen Marz <jamenmarz@gmail.com>
2015-12-17 06:49:27 -0700

    Clean

--- main.js
+++ main.js
@@ -11,19 +11,5 @@ import BrowserWindow from 'browser-window';
 let main = app.main = {};
 
 app.on('ready', () => {
-  main = app.main = new BrowserWindow(neta.main);
-  main.loadURL('file://' + join(__dirname, '/index.html'));
-
-  fs.readdir(neta.dirs.packages, (err, packages) => {
-    if (!err) {
-      neta.load(packages);
-      if (main.webContents.isLoading()) {
-        main.webContents.on('dom-ready', () => neta.load(packages));
-      } else {
-        neta.load(packages);
-      }
-    } else {
-      console.warn('Could not load packages');
-    }
-  });
+  
 });
