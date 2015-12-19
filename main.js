5a8a3333909af37d3096a882fd0d593118d1c79b
Jamen Marz <jamenmarz@gmail.com>
2015-12-19 02:23:05 -0700

    Add CLI + dev param

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 import {app, BrowserWindow} from 'electron';
 import {join} from 'path';
 import neta from './lib';
+import minimist from 'minimist';
 
+let args = neta.args = minimist(process.argv.slice(2));
 let main = neta.main = {};
 
 app.on('ready', () => {
   main = neta.main = new BrowserWindow(neta.mainWindow());
   main.loadURL('file://' + join(__dirname, 'view', 'app.html'));
+
+  // Developer
+  if (args.dev) {
+    main.openDevTools({detach: true});
+  }
 });
 
 app.on('window-all-closed', function() {
