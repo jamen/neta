c5e6570bf792084c7a987b5093b9fd473ed81a13
Jamen Marz <jamenmarz@gmail.com>
2015-12-19 01:42:59 -0700

    Rename main() -> mainWindow()

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import {app, BrowserWindow} from 'electron';
 import {join} from 'path';
 import neta from './lib';
 
-let main = app.main = {};
+let main = neta.main = {};
 
 app.on('ready', () => {
-  main = app.main = new BrowserWindow(neta.main());
+  main = neta.main = new BrowserWindow(neta.main());
   main.loadURL('file' + join(__dirname, 'views', 'app.html'));
 });
 
