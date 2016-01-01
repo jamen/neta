452df529cfdc4cb6fb5d453cf7b37bddef4d32d2
Jamen Marzonie <jamenmarz@gmail.com>
2015-12-31 20:24:46 -0700

    Update packages

--- app/paths.js
+++
@@ -1,5 +0,0 @@
-import { resolve, join } from 'path';
-
-export const base = resolve(process.env.HOME, '.neta');
-export const settings = join(base, 'settings.json');
-export const packages = join(base, 'packages');
--- index.js
+++
@@ -1,7 +0,0 @@
-/* index.js
- * Bootstrap file.
- * If you're looking for the Main-Process file, see main.js
- * * */
-
-require('babel-register');
-require('./main.js');
