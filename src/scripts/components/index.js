74730431936a35ea6cce977be29aaf0082892a93
Jamen Marzonie <jamenmarz@gmail.com>
2016-02-23 16:39:51 -0700

    Fix exporting

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -3,4 +3,6 @@ import Bar from './bar';
 import BarSect from './bar-sect';
 import BarButton from './bar-button';
 import Chat from './chat';
-export { App, Bar, BarSect, BarButton, Chat };
+import Home from './home';
+import Settings from './settings';
+export { App, Bar, BarSect, BarButton, Chat, Home, Settings };
